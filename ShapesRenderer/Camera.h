646ff38f50af190e21b573b9bc2b2664a3fc37ec
chiappa <chiappa@raa-st.com>
2022-04-15 02:21:43 +0300

    --

---
+++ ShapesRenderer/Camera.h
@@ -0,0 +1,205 @@
+#pragma once
+
+#include <glm/gtc/matrix_transform.hpp>
+const float PITCH = 0.0f;
+const float SPEED = 0.1f;
+const float SENSITIVTY = 0.25f;
+const float ZOOM = 45.0f;
+
+using Vec3 = glm::vec3;
+using Vec4 = glm::vec4;
+using Mat4 = glm::mat4;
+
+struct STransform
+{
+	Vec3 Position;
+	Vec3 Rotation;
+	Vec3 Scale;
+};
+
+enum class Movement
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
+class CCamera
+{
+public:
+
+	enum class Mode
+	{
+		FPS,
+		FLY
+	} mode = CCamera::Mode::FLY;
+	enum class Type
+	{
+		Perspective,
+		Ortho
+	} type = Type::Perspective;
+
+public:
+	// Camera Attributes
+	STransform    transform;
+	Vec3 Front;
+	Vec3 Up;
+	Vec3 Right;
+	Vec3 WorldUp;
+	Vec3        m_MoveDirection;
+	// Camera options
+	float        MovementSpeed;
+	float        Zoom;
+	float        m_fov = 45.0f;
+	float        Ratio = 16.0f / 9;
+	float        zNear = 0.1f;
+	float        zFar = 10000.f;
+
+	Vec4 ViewPort;
+
+	// Constructor with vectors
+	CCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.f, float pitch = PITCH, Vec3 Front = Vec3{ 0,0,1 })
+		: MovementSpeed(10.f)
+		, Zoom(ZOOM)
+	{
+		this->transform.Position = position;
+		this->WorldUp = up;
+		this->transform.Rotation.y = yaw;
+		this->transform.Rotation.x = pitch;
+		this->updateCameraVectors();
+	}
+	// Constructor with scalar values
+	CCamera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+		: Front(glm::vec3(0.0f, 0.0f, 0.0f))
+		, MovementSpeed(10.f)
+		, Zoom(ZOOM)
+	{
+		this->transform.Position = glm::vec3(posX, posY, posZ);
+		this->WorldUp = glm::vec3(upX, upY, upZ);
+		this->transform.Rotation.y = yaw;
+		this->transform.Rotation.x = pitch;
+		this->updateCameraVectors();
+	}
+
+	void Update()
+	{
+		updateCameraVectors();
+	}
+
+	void Init(int width, int height, float fov = 45.f)
+	{
+	}
+	void SetAngle(Vec3 ang)
+	{
+	}
+	// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+	// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+	Mat4 GetViewMatrix() const
+	{
+		//return glm::lookAt(this->transform.position, this->transform.position + this->Front, this->Up);
+		return glm::lookAtLH(this->transform.Position, this->transform.Position + this->Front, this->Up);
+	}
+	void SetViewport(const Vec4& viewPort) { ViewPort = viewPort; }
+	Mat4 GetProjectionMatrix()
+	{
+		Vec4& v = ViewPort;
+		return glm::perspective(glm::radians(m_fov), (float)(v.z) / (float)(v.w), zNear, zFar);
+	}
+
+	Vec3 GetPos() const
+	{
+		return transform.Position;
+	}
+	Vec3 GetAngles() const
+	{
+		return transform.Rotation;
+	}
+
+	void SetPos(glm::vec3 pos)
+	{
+		transform.Position = pos;
+	}
+
+	void SetAngles(glm::vec3 ang)
+	{
+		transform.Rotation = ang;
+	}
+
+	inline void  SetFov(float fov) { m_fov = fov; }
+	inline float GetFov() const { return (m_fov); }
+
+	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+	void         ProcessMouseScroll(float yoffset)
+	{
+		if (this->Zoom >= 1.0f && this->Zoom <= 45.0f)
+			this->Zoom -= yoffset;
+		if (this->Zoom <= 1.0f)
+			this->Zoom = 1.0f;
+		if (this->Zoom >= 45.0f)
+			this->Zoom = 45.0f;
+	}
+	void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true)
+	{
+		transform.Rotation.y -= xoffset;
+		transform.Rotation.x += yoffset;
+
+		// Make sure that when pitch is out of bounds, screen doesn't get flipped
+		if (constrainPitch)
+		{
+			auto& x = transform.Rotation.x;
+			x = glm::clamp(x, -89.f, 89.f);
+		}
+
+		// Update Front, Right and Up Vectors using the updated Eular angles
+		updateCameraVectors();
+	}
+	void ProcessKeyboard(Movement direction, float deltaTime, float value)
+	{
+		float velocity = deltaTime * value;
+		mode = CCamera::Mode::FLY;
+
+		auto moveForward = glm::vec3(
+			Front.x,
+			mode == CCamera::Mode::FPS ? 0 : Front.y,
+			Front.z);
+
+		if (direction == Movement::FORWARD)
+		{
+			m_MoveDirection = moveForward;
+		}
+		if (direction == Movement::BACKWARD)
+		{
+			m_MoveDirection = -moveForward;
+		}
+		if (direction == Movement::LEFT)
+		{
+			m_MoveDirection = Right;
+		}
+		if (direction == Movement::RIGHT)
+		{
+			m_MoveDirection = -Right;
+		}
+		//m_MoveDirection = glm::normalize(m_MoveDirection);
+		//m_MoveDirection *= velocity;
+		transform.Position += velocity * m_MoveDirection;
+		//minecraft->player.move(m_MoveDirection, MovementSpeed * velocity);
+	}
+
+
+	// Calculates the front vector from the Camera's (updated) Eular Angles
+	void updateCameraVectors()
+	{
+		// Calculate the new Front vector
+		glm::vec3 front;
+		front.x = cos(glm::radians(this->transform.Rotation.y)) * cos(glm::radians(this->transform.Rotation.x));
+		front.y = sin(glm::radians(this->transform.Rotation.x));
+		front.z = sin(glm::radians(this->transform.Rotation.y)) * cos(glm::radians(this->transform.Rotation.x));
+		this->Front = glm::normalize(front);
+		// Also re-calculate the Right and Up vector
+		this->Right = glm::normalize(glm::cross(this->Front, this->WorldUp)); // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+		this->Up = glm::normalize(glm::cross(this->Right, this->Front));
+	}
+};
--- ShapesRenderer/ShapesRenderer.cpp
+++ ShapesRenderer/ShapesRenderer.cpp
@@ -4,18 +4,15 @@
 #include "ShapesRenderer.h"
 #include <vector>
 #include <glm/glm.hpp>
-#include <gl/GL.h>
+
+#include <imgui.h>
+#include <imgui_impl_opengl2.h>
+#include <imgui_impl_sdl.h>
+
+#include "Camera.h"
 
 namespace Test
 {
-	using Vec3 = glm::vec3;
-
-	struct STransform
-	{
-		Vec3 Position;
-		Vec3 Rotation;
-		Vec3 Scale;
-	};
 
 	struct SDrawParams
 	{
@@ -31,10 +28,10 @@ namespace Test
 
 	struct SShape
 	{
-		IShape*    Shape;
+		IShape* Shape;
 		Vec3       Color;
 		STransform Transform;
-		void       Draw() { Shape->Draw({Transform, Color}); }
+		void       Draw() { Shape->Draw({ Transform, Color }); }
 	};
 
 	class CBaseShape : IShape
@@ -47,6 +44,7 @@ namespace Test
 
 	int CreateShaderFromDisk(std::string_view Path)
 	{
+		return 0;
 	}
 
 	float GetTime()
@@ -63,7 +61,7 @@ namespace Test
 		}
 		void DrawShapes()
 		{
-			for each (const auto& shape in m_Shapes)
+			for (const auto& shape : m_Shapes)
 			{
 				shape->Draw();
 			}
@@ -71,7 +69,7 @@ namespace Test
 		void Update()
 		{
 			auto t = GetTime();
-			for each (auto shape in m_Shapes)
+			for (auto shape : m_Shapes)
 			{
 				shape->Color = glm::sin(Vec3(t, t, t));
 			}
@@ -106,11 +104,196 @@ namespace Test
 			Renderer.DrawShapes();
 		}
 
+		return 0;
 	}
 } // namespace Test
 
-int main()
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int32_t b32;
+
+#define WinWidth 1000
+#define WinHeight 1000
+
+int main(int ArgCount, char** Args)
 {
-	cout << "Hello CMake." << endl;
+
+	u32 WindowFlags = SDL_WINDOW_OPENGL;
+	SDL_Window* Window = SDL_CreateWindow("OpenGL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WinWidth, WinHeight, WindowFlags);
+	assert(Window);
+	SDL_GLContext Context = SDL_GL_CreateContext(Window);
+
+	b32 Running = 1;
+	b32 FullScreen = 0;
+
+	CCamera Camera({ 0,0,0 }, { 0,1,0 }, 0, 0);
+	Camera.SetViewport(Vec4(0, 0, WinWidth, WinHeight));
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+
+	auto ctx = ImGui::CreateContext();
+
+	auto quadric = gluNewQuadric();
+
+	bool mouseDown[2] = { false };
+
+	bool Keys[512] = {};
+	auto isDown = [&Keys](int K) {
+		return Keys[K];
+	};
+
+	auto isLeft = [&isDown]() {return isDown(SDLK_a); };
+	auto isRight = [&isDown]() {return isDown(SDLK_d); };
+	auto isFront = [&isDown]() {return isDown(SDLK_w); };
+	auto isBack = [&isDown]() {return isDown(SDLK_s); };
+
+	float mouseSensitivity = 0.2;
+	//******************************************* // Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplSDL2_InitForOpenGL(Window, Context);
+	ImGui_ImplOpenGL2_Init();
+
+	// Our state
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+	while (Running)
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glMultMatrixf(&Camera.GetProjectionMatrix()[0][0]);
+
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glMultMatrixf(&Camera.GetViewMatrix()[0][0]);
+
+
+		SDL_Event Event;
+		while (SDL_PollEvent(&Event))
+		{
+			ImGui_ImplSDL2_ProcessEvent(&Event);
+			if (Event.type == SDL_KEYDOWN)
+			{
+				Keys[Event.key.keysym.sym] = true;
+			}
+			if (Event.type == SDL_KEYUP)
+			{
+				Keys[Event.key.keysym.sym] = false;
+			}
+			if (Event.type == SDL_KEYDOWN)
+			{
+				switch (Event.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					Running = 0;
+					break;
+				case 'f':
+					FullScreen = !FullScreen;
+					if (FullScreen)
+					{
+						SDL_SetWindowFullscreen(Window, WindowFlags | SDL_WINDOW_FULLSCREEN_DESKTOP);
+					}
+					else
+					{
+						SDL_SetWindowFullscreen(Window, WindowFlags);
+					}
+					break;
+				default:
+					break;
+				}
+			}
+			if (!ImGui::GetIO().WantCaptureMouse)
+			{
+				if (Event.type == SDL_MOUSEBUTTONDOWN)
+				{
+					mouseDown[Event.button.button] = true;
+				}
+				else if (Event.type == SDL_MOUSEBUTTONUP)
+				{
+					mouseDown[Event.button.button] = false;
+				}
+				if (Event.type == SDL_MOUSEMOTION && mouseDown[1])
+				{
+					auto x = mouseSensitivity * Event.motion.xrel;
+					auto y = mouseSensitivity * Event.motion.yrel;
+					Camera.ProcessMouseMovement(x, y);
+				}
+			}
+			if (isLeft()) { Camera.ProcessKeyboard(Movement::LEFT, 0.016, 1); }
+			if (isRight()) { Camera.ProcessKeyboard(Movement::RIGHT, 0.016, 1); }
+			if (isFront()) { Camera.ProcessKeyboard(Movement::FORWARD, 0.016, 1); }
+			if (isBack()) { Camera.ProcessKeyboard(Movement::BACKWARD, 0.016, 1); }
+
+			else if (Event.type == SDL_QUIT)
+			{
+				Running = 0;
+			}
+		}
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL2_NewFrame();
+		ImGui_ImplSDL2_NewFrame();
+		ImGui::NewFrame();
+
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+		//if (show_demo_window)
+		//	ImGui::ShowDemoWindow(&show_demo_window);
+		{
+			ImGui::DragFloat3("CameraPosition", &Camera.transform.Position[0]);
+			ImGui::SliderAngle("CameraRotation", &Camera.transform.Rotation[0]);
+
+		}
+
+		// Rendering
+		ImGui::Render();
+		glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+		glPushMatrix();
+		//glScalef(0.5f, 0.5f, 0.5f);
+		glTranslatef(0, 0, 0);
+
+		glColor4f(1, 1, 1, 1);
+		glBegin(GL_QUADS);                      // Draw A Quad
+		glVertex3f(-1.0f, 1.0f, 0.0f);              // Top Left
+		glVertex3f(1.0f, 1.0f, 0.0f);              // Top Right
+		glVertex3f(1.0f, -1.0f, 0.0f);              // Bottom Right
+		glVertex3f(-1.0f, -1.0f, 0.0f);              // Bottom Left
+		glEnd();
+		glPopMatrix();
+
+
+		gluQuadricDrawStyle(quadric, GLU_FILL); // устанавливаем
+						// стиль: сплошной
+		gluSphere(quadric, 0, 10, 10);
+
+		ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+		SDL_GL_SwapWindow(Window);
+	}
+	gluDeleteQuadric(quadric);
 	return 0;
 }
